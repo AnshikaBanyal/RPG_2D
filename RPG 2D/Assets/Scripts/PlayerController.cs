93cc0fd300087766eb4861846ff5e67897927d54
AnshikaBanyal <156026659+AnshikaBanyal@users.noreply.github.com>
2025-06-25 04:19:03 +0530

    player animation(idle and running)
    2/51

--- RPG 2D/Assets/Scripts/PlayerController.cs
+++ RPG 2D/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f; // Speed of the player movement
 
     private PlayerControls playerControls; // Reference to the PlayerControls class
-    private Vector2 movement; //movement of the player direction and speed
     private Rigidbody2D rb;
+    private SpriteRenderer mySpriteRenderer;
+    private Animator myAnimator;
+
+    private Vector2 movement; //movement of the player direction and speed
 
     // Start is called before the first frame update
     //Awake calles when the script instance is being loaded
@@ -15,6 +18,8 @@ public class PlayerController : MonoBehaviour
     {
         playerControls = new PlayerControls(); // Initialize the PlayerControls instance
         rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to the player
+        mySpriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component for visual representation
+        myAnimator = GetComponent<Animator>(); // Get the Animator component for animations
     }
 
     private void OnEnable()
@@ -30,6 +35,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //AdjustPlayerFacingDirection(); // Adjust the player's facing direction based on mouse position
         Move(); // Call the method to move the player based on input
     }
 
@@ -37,6 +43,16 @@ public class PlayerController : MonoBehaviour
     {
         // Get the movement input from the PlayerControls instance
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        myAnimator.SetFloat("moveX", movement.x); // Set the horizontal movement parameter for the animator
+        myAnimator.SetFloat("moveY", movement.y); // Set the vertical movement parameter for the animator
+        if (movement.x < 0)
+        {
+            mySpriteRenderer.flipX = true; // Flip the sprite horizontally if moving left
+        }
+        else if (movement.x > 0)
+        {
+            mySpriteRenderer.flipX = false; // Do not flip the sprite if moving right
+        }
     }
 
     private void Move()
@@ -47,4 +63,19 @@ public class PlayerController : MonoBehaviour
         // Move the player by applying the movement vector to the Rigidbody2D
         rb.MovePosition(rb.position + moveDirection);
     }
+
+    //from the tutorial, but i dont like so i will not use it and make my own... but writing it here in case i need it inthe future and can't avoid it
+    /*private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition; // Get the mouse position in screen space
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position); // Convert player position to screen space
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true; // Flip the sprite if the mouse is to the left of the player
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false; // Do not flip the sprite if the mouse is to the right of the player
+        }
+    }*/
 }
